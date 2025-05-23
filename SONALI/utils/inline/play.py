f6165c0bd2db70ff2c8c6abe7cf8bda650556d13
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-05-23 15:15:34 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -40,24 +40,24 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "🅢—————————"
+        bar = "Ⓐ—————————"
     elif 10 < umm < 20:
-        bar = "—🅞————————"
+        bar = "—Ⓝ————————"
     elif 20 <= umm < 30:
-        bar = "——🅝———————"
+        bar = "——Ⓖ———————"
     elif 30 <= umm < 40:
-        bar = "———🅐——————"
+        bar = "———Ⓡ——————"
     elif 40 <= umm < 50:
-        bar = "————♡—————"
+        bar = "————Ⓨ—————"
     elif 50 <= umm < 60:
-        bar = "—————🅡————"
+        bar = "—————Ⓑ————"
     elif 60 <= umm < 70:
-        bar = "——————🅐———"
+        bar = "——————Ⓘ———"
     elif 70 <= umm < 80:
-        bar = "———————🅙——"
+        bar = "———————Ⓡ——"
     elif 80 <= umm < 95:
-        bar = "————————🅐—"
-    else:
+        bar = "————————Ⓓ—"
+    else:ⒶⓃⒼⓇⓎ ⒷⒾⓇⒹ
         bar = "——————————♡"
 
     buttons = [
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text=" 𝗝ᴏɪɴ 🤌 ", url="https://t.me/PURVI_SUPPORT",
+                text=" 𝗝ᴏɪɴ 🤌 ", url="https://t.me/Duniya_ki_Telegram",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/PURVI_UPDATES",
+                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/TG_TELEGRAM_KI_DUNIYA",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -99,10 +99,10 @@ def stream_markup(_, videoid, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝗝ᴏɪɴ 🤌", url="https://t.me/PURVI_SUPPORT",
+                text="𝗝ᴏɪɴ 🤌", url="https://t.me/Duniya_ki_Telegram",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/PURVI_UPDATES",
+                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/TG_TELEGRAM_KI_DUNIYA",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
